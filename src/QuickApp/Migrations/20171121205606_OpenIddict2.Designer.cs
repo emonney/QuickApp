39b1cf24167c369b7cd349335e5c9ca4e677a042
Jason K. Carter <carter.jk@gmail.com>
2017-11-21 16:25:21 -0600

    Upgrade to OpenIddict 2.0.0-rc1-final and AspNet.Security.OAuth.Validation 2.0.0-rc1-final

--- src/QuickApp/Migrations/20171121205606_OpenIddict2.Designer.cs
+++ src/QuickApp/Migrations/20171121205606_OpenIddict2.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace QuickApp.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20171121205606_OpenIddict2")]
+    partial class OpenIddict2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
