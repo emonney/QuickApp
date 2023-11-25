715f8889376ebb4fb3f78c7a60dc4d4798364374
Ebenezer Monney <ebenmonney@gmail.com>
2023-11-25 18:58:35 +0000

    .net 8 ng 17 rough update

--- QuickApp/Helpers/Extensions.cs
+++ QuickApp/Helpers/Extensions.cs
@@ -14,19 +14,6 @@ namespace QuickApp.Helpers
 {
     public static class Extensions
     {
-        // HttpResponse
-        public static void AddPagination(this HttpResponse response, int currentPage, int itemsPerPage, int totalItems, int totalPages)
-        {
-            response.Headers.Add("Pagination", JsonConvert.SerializeObject(new PageHeader(currentPage, itemsPerPage, totalItems, totalPages)));
-            response.Headers.Add("access-control-expose-headers", "Pagination"); // CORS
-        }
-
-        public static void AddApplicationError(this HttpResponse response, string message)
-        {
-            response.Headers.Add("Application-Error", message);
-            response.Headers.Add("access-control-expose-headers", "Application-Error");// CORS
-        }
-
         // string
         public static string NullIfWhiteSpace(this string value)
         {
@@ -36,7 +23,7 @@ namespace QuickApp.Helpers
         // Array
         public static T[] NullIfEmpty<T>(this T[] value)
         {
-            return value?.Any() == true ? value : null;
+            return value?.Length == 0 ? null : value;
         }
     }
 }
